cb6ae1147e23dd86fa61b10ffc19253632ce21b1
PriyanshuKadam <142070169+PriyanshuKadam@users.noreply.github.com>
2024-09-27 22:31:59 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -3,14 +3,11 @@ import pandas as pd
 from data import transform_text
 from model import predict
 
-# Load training data
-train = pd.read_csv('../data/train.csv')
+train = pd.read_csv('../data/train.csv') 
 
-# Streamlit app
 st.title("Disaster Prediction from Tweets")
 input_text = st.text_area("Enter your tweet text here:")
 
-# Check if input is provided
 if st.button("Predict"):
     if input_text:
         X, y, transformed_input = transform_text(train, input_text)
